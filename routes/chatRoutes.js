7853e479627e47336a1d1eb9d7e02cd43e2582a5
jurek.ohrndorf <jurek.ohrndorf@check24.de>
2024-09-23 01:50:00 +0200

    standard chat route

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -3,6 +3,10 @@ import { getMessages } from "../controllers/chatController.js";
 
 const chatRouter = express.Router();
 
+chatRouter.get("/", (req, res) => {
+  res.json({ message: "Success" });
+});
+
 chatRouter.get("/messages", getMessages);
 
 export default chatRouter;
